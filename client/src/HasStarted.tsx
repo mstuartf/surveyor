44ab96a1979063cbd5ddc3c19932ab49a275f36d
mikef <mstuartflood@gmail.com>
2020-03-15 10:44:58 +0000

    delete duplicated code

--- client/src/HasStarted.tsx
+++ client/src/HasStarted.tsx
@@ -1,8 +1,4 @@
 import { gql } from "apollo-boost";
-import { useQuery } from "@apollo/react-hooks";
-import Question from "./Question";
-import { Redirect } from "react-router";
-import React from "react";
 
 export const HAS_ANON_USER = gql`
   query HasAnonUser {
@@ -10,22 +6,3 @@ export const HAS_ANON_USER = gql`
     direction @client
   }
 `;
-
-const HasStarted = props => {
-  const { data } = useQuery(HAS_ANON_USER);
-  const { questionId, surveyId } = props.match.params;
-
-  if (data && data.anonUserId) {
-    return (
-      <Question
-        anonUserId={data.anonUserId}
-        questionId={questionId}
-        direction={data.direction}
-      />
-    );
-  }
-
-  return <Redirect to={`/survey/${surveyId}`} />;
-};
-
-export default HasStarted;
--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,29 +1,7 @@
-import React, { useState } from "react";
-import CardStack from "./components/CardStack/CardStack";
+import React from "react";
 
 const Home = () => {
-  const [[val, direction], setPage] = useState([0, 0]);
-
-  const paginate = (direction: number) => {
-    setPage([val + direction, -direction]);
-  };
-
-  return (
-    <div className="border border-dashed border-gray-300 rounded overflow-hidden p-8 w-full max-w-lg h-full max-h-2xl m-auto mt-16">
-      <div className="w-full h-full relative">
-        <CardStack
-          val={val}
-          direction={direction}
-          nextCard={() => paginate(1)}
-          previousCard={() => paginate(-1)}
-        >
-          <div className="w-full h-full border border-gray-300 rounded bg-white box-border shadow-md">
-            {val}
-          </div>
-        </CardStack>
-      </div>
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
