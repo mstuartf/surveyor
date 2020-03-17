5d82b59420afca4959f86a57b7352aae405db2f2
mikef <mstuartflood@gmail.com>
2020-03-17 19:38:58 +0000

    simple debounce input component

--- client/src/components/SingleInput/SingleInput.tsx
+++ client/src/components/SingleInput/SingleInput.tsx
@@ -1,9 +1,30 @@
 import React from "react";
 
 const SingleInput = ({ minValuesReminder, minValues, answer, onSave }) => {
+  let timer;
+
+  const saveValueDebounce = (value: string) => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(() => {
+      saveValue(value);
+    }, 300);
+  };
+
+  const saveValue = (value: string) => {
+    onSave([value]);
+  };
+
+  const value = answer ? answer.values[0] : "";
+
   return (
     <>
       <div className="mt-2">Input answer</div>
+      <input
+        value={value}
+        onChange={event => saveValueDebounce(event.target.value)}
+      />
     </>
   );
 };
