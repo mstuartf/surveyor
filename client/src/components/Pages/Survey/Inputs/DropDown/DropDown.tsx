c7cb939755621d2c43d892bc70c34f3165a684e0
mikef <mstuartflood@gmail.com>
2020-03-22 17:31:53 +0000

    dropdown styling

--- client/src/components/Pages/Survey/Inputs/DropDown/DropDown.tsx
+++ client/src/components/Pages/Survey/Inputs/DropDown/DropDown.tsx
@@ -1,7 +1,6 @@
 import React, { ChangeEvent } from "react";
 import { useDropDownQueryQuery } from "../../../../../generated/graphql";
 import { sortResource } from "../../../../../pagination";
-import ReminderText from "../../../../Generic/ReminderText";
 
 interface Props {
   questionId: string;
@@ -24,13 +23,9 @@ const DropDown = ({ questionId, onSave }: Props) => {
   const value = answer ? answer.values[0] : "";
 
   return (
-    <>
-      <div className="flex items-center justify-center py-4">
-        <ReminderText>This question is required!</ReminderText>
-      </div>
-      <div className="mt-2">Select answers:</div>
+    <div className={"py-4"}>
       <select
-        className="flex flex-col mt-2"
+        className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
         onChange={e => selectValue(e)}
         value={value}
       >
@@ -41,7 +36,16 @@ const DropDown = ({ questionId, onSave }: Props) => {
           </option>
         ))}
       </select>
-    </>
+      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+        <svg
+          className="fill-current h-4 w-4"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+        >
+          <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+        </svg>
+      </div>
+    </div>
   );
 };
 
